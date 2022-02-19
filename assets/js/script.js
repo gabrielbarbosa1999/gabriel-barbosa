c5b744666d7f64317cb9c2cebbac6a035c317495
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2022-02-19 10:34:38 -0300

    Site Gabriel Barbosa

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ function openMenu() {
     let menu = document.querySelector('#menu-area');
     let opener = document.querySelector('.fas');
 
-    if(menu.style.width == '50vh') {
+    if(menu.style.width == '50vw') {
         menu.style.width = '0px';
         opener.classList.add('fa-bars');
         opener.classList.remove('fa-times');
     } else {
-        menu.style.width = '50vh';
+        menu.style.width = '50vw';
         opener.classList.remove('fa-bars');
         opener.classList.add('fa-times');
     }
